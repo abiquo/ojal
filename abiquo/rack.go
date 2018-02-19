3ab6eb3b735c8812fb91c12608f7bc4921940f6f
antxon <antxon.gonzalez@abiquo.com>
2018-02-19 11:39:13 +0100

    Add rack resource

---
+++ abiquo/rack.go
@@ -0,0 +1,16 @@
+package abiquo
+
+import "github.com/abiquo/opal/core"
+
+type Rack struct {
+	HAEnabled bool   `json:"haEnabled,omitempty"`
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name"`
+	NRSQ      int    `json:"nrsq,omitempty"`
+	VlanIDMax int    `json:"vlanIdMax,omitempty"`
+	VlanIDMin int    `json:"vlanIdMin,omitempty"`
+	Reserved  int    `json:"vlanPerVdcReserved,omitempty"`
+	core.DTO
+}
+
+func newRack() core.Resource { return new(Rack) }
