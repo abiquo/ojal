b7925031a81149fb9a9cef221b207efb2eda3cfb
antxon <antxon.gonzalez@abiquo.com>
2018-02-28 15:36:55 +0100

    Simplify initialization. Do not use dot import

--- abiquo/examples_test.go
+++ abiquo/examples_test.go
@@ -6,8 +6,8 @@ import (
 	"sort"
 	"time"
 
-	. "github.com/abiquo/ojal/abiquo"
-	. "github.com/abiquo/ojal/core"
+	"github.com/abiquo/ojal/abiquo"
+	"github.com/abiquo/ojal/core"
 )
 
 var (
@@ -27,27 +27,21 @@ func init() {
 			panic(k + " environment variable should not be empty")
 		}
 	}
-}
 
-func ExampleInit() {
-	err := Abiquo(environment["OPAL_ENDPOINT"], Basic{
+	if err := core.Init(environment["OPAL_ENDPOINT"], core.Basic{
 		Username: environment["OPAL_USERNAME"],
 		Password: environment["OPAL_PASSWORD"],
-	})
-
-	fmt.Println(err)
-	fmt.Println()
-
-	// Output:
-	// <nil>
+	}); err != nil {
+		panic(err)
+	}
 }
 
 // ExampleCollection shows how to retrieve the users name from an enterpirse
 func ExampleCollection() {
 	users := []string{}
-	collection := NewLinker("admin/enterprises/1/users", "users").Collection(nil)
+	collection := core.NewLinker("admin/enterprises/1/users", "users").Collection(nil)
 	for collection.Next() {
-		users = append(users, collection.Item().(*User).Name)
+		users = append(users, collection.Item().(*abiquo.User).Name)
 	}
 	sort.Strings(users)
 	fmt.Println(users)
@@ -58,8 +52,8 @@ func ExampleCollection() {
 
 // ExampleCategories shows how to list all the categories
 func ExampleCategories() {
-	category := Categories(nil).Find(func(r Resource) bool {
-		return r.(*Category).Name == "Others"
+	category := abiquo.Categories(nil).Find(func(r core.Resource) bool {
+		return r.(*abiquo.Category).Name == "Others"
 	})
 	fmt.Println(category != nil)
 
@@ -74,9 +68,9 @@ func ExampleCategories() {
 
 // ExampleCategory shows how to create a category
 func ExampleCategory() {
-	category := &Category{Name: name, Erasable: true}
+	category := &abiquo.Category{Name: name, Erasable: true}
 	fmt.Println(category.Create())
-	fmt.Println(Remove(category))
+	fmt.Println(core.Remove(category))
 
 	// Output:
 	// <nil>
@@ -84,7 +78,7 @@ func ExampleCategory() {
 }
 
 func ExampleLogin() {
-	user := Login()
+	user := abiquo.Login()
 	enterprise := user.Enterprise()
 
 	fmt.Println(user == nil)
@@ -100,33 +94,33 @@ func ExampleLogin() {
 }
 
 func ExampleNetwork() {
-	location := NewLinkType("admin/datacenters/1", "location")
-	datacenter := NewLinkType("cloud/locations/1", "location")
+	location := core.NewLinkType("admin/datacenters/1", "location")
+	datacenter := core.NewLinkType("cloud/locations/1", "location")
 
-	enterprise := &Enterprise{Name: name}
+	enterprise := &abiquo.Enterprise{Name: name}
 	err0 := enterprise.Create()
-	err1 := enterprise.CreateLimit(&Limit{DTO: NewDTO(
+	err1 := enterprise.CreateLimit(&abiquo.Limit{DTO: core.NewDTO(
 		enterprise.Link().SetRel("enterprise"),
 		location.SetRel("location"),
 	)})
 
-	vdc := &VirtualDatacenter{
+	vdc := &abiquo.VirtualDatacenter{
 		Name:   name,
 		HVType: "KVM",
-		Network: &Network{
+		Network: &abiquo.Network{
 			Mask:    24,
 			Address: "192.168.0.0",
 			Gateway: "192.168.0.1",
 			Name:    name,
 			TypeNet: "INTERNAL",
 		},
-		DTO: NewDTO(
+		DTO: core.NewDTO(
 			datacenter.SetRel("location"),
 			enterprise.Link().SetRel("enterprise"),
 		),
 	}
 	err2 := vdc.Create()
-	err3 := vdc.Network.CreateIP(&IP{IP: "192.168.0.253"})
+	err3 := vdc.Network.CreateIP(&abiquo.IP{IP: "192.168.0.253"})
 
 	fmt.Println(err0)
 	fmt.Println(err1)
@@ -142,24 +136,24 @@ func ExampleNetwork() {
 
 // ExampleDatacenter shows the Datacenter functionality
 func ExampleDatacenter() {
-	dc := new(Datacenter)
-	endpoint := NewLinkType("admin/datacenters/1", "datacenter")
-	read := Read(endpoint, dc)
-	network := &Network{
+	dc := new(abiquo.Datacenter)
+	endpoint := core.NewLinkType("admin/datacenters/1", "datacenter")
+	read := core.Read(endpoint, dc)
+	network := &abiquo.Network{
 		Mask:    24,
 		Address: "172.16.45.0",
 		Gateway: "172.16.45.1",
 		Name:    name,
 		Tag:     3743,
 		TypeNet: "EXTERNAL",
-		DTO: NewDTO(
-			NewLinkType("admin/enterprises/1", "enterprise").SetRel("enterprise"),
-			NewLinkType("admin/datacenters/1/networkservicetypes/1", "networkservicetype").SetRel("networkservicetype"),
+		DTO: core.NewDTO(
+			core.NewLinkType("admin/enterprises/1", "enterprise").SetRel("enterprise"),
+			core.NewLinkType("admin/datacenters/1/networkservicetypes/1", "networkservicetype").SetRel("networkservicetype"),
 		),
 	}
 	fmt.Println(read)
 	fmt.Println(dc.CreateExternal(network))
-	fmt.Println(Remove(network))
+	fmt.Println(core.Remove(network))
 
 	// Output:
 	// <nil>
@@ -168,11 +162,11 @@ func ExampleDatacenter() {
 }
 
 func ExampleVirtualMachine_Deploy() {
-	endpoint := NewLink("cloud/virtualdatacenters/1/virtualappliances/1")
-	template := NewLinkType("admin/enterprises/1/datacenterrepositories/1/virtualmachinetemplates/1", "virtualmachinetemplate")
-	vapp := endpoint.SetType("virtualappliance").Walk().(*VirtualAppliance)
-	vm := &VirtualMachine{
-		DTO: NewDTO(template.SetRel("virtualmachinetemplate")),
+	endpoint := core.NewLinkType("cloud/virtualdatacenters/1/virtualappliances/1", "virtualappliance")
+	template := core.NewLinkType("admin/enterprises/1/datacenterrepositories/1/virtualmachinetemplates/1", "virtualmachinetemplate")
+	vapp := endpoint.Walk().(*abiquo.VirtualAppliance)
+	vm := &abiquo.VirtualMachine{
+		DTO: core.NewDTO(template.SetRel("virtualmachinetemplate")),
 	}
 
 	vapp.CreateVM(vm)
