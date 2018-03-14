958b46e9f7f30a1a538af6b1fd6992ebf4d61046
antxon <antxon.gonzalez@abiquo.com>
2018-03-14 19:02:30 +0100

    Use go friendly names

--- abiquo/datastore.go
+++ abiquo/datastore.go
@@ -3,23 +3,23 @@ package abiquo
 import "github.com/abiquo/ojal/core"
 
 type Datastore struct {
-	DatastoreUUID string `json:"datastoreUUID"`
-	Directory     string `json:"directory"`
-	Enabled       bool   `json:"enabled"`
-	ID            int    `json:"id,omitempty"`
-	Name          string `json:"name"`
-	RootPath      string `json:"rootPath"`
-	Size          int64  `json:"size"`
-	UsedSize      int64  `json:"usedSize"`
+	UUID      string `json:"datastoreUUID"`
+	Directory string `json:"directory"`
+	Enabled   bool   `json:"enabled"`
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name"`
+	RootPath  string `json:"rootPath"`
+	Size      int64  `json:"size"`
+	UsedSize  int64  `json:"usedSize"`
 	core.DTO
 }
 
 type DatastoreTier struct {
-	DefaultAllowed          bool   `json:"defaultAllowed"`
-	DefaultForDatacenter    bool   `json:"defaultForDatacenter"`
-	Description             string `json:"description"`
-	Enabled                 bool   `json:"enabled"`
-	Name                    string `json:"name"`
-	StorageAllocationPolicy string `json:"storageAllocationPolicy"`
+	DefaultAllowed       bool   `json:"defaultAllowed"`
+	DefaultForDatacenter bool   `json:"defaultForDatacenter"`
+	Description          string `json:"description"`
+	Enabled              bool   `json:"enabled"`
+	Name                 string `json:"name"`
+	Policy               string `json:"storageAllocationPolicy"`
 	core.DTO
 }
