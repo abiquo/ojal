8f7c184ba63be770b39796e9ab8a621259d56ae1
antxon <antxon.gonzalez@abiquo.com>
2018-03-28 15:40:10 +0200

    Register missing resources/collections

--- abiquo/abiquo.go
+++ abiquo/abiquo.go
@@ -6,6 +6,8 @@ import (
 
 func init() {
 	collections := map[string]string{
+		"alarm":                  "alarms",
+		"alert":                  "alerts",
 		"backuppolicy":           "backuppolicies",
 		"category":               "categories",
 		"datacenter":             "datacenters",
@@ -16,10 +18,13 @@ func init() {
 		"device":                 "devices",
 		"devicetype":             "devicetypes",
 		"enterprise":             "enterprises",
+		"firewallpolicy":         "firewallpolicies",
 		"fitpolicyrule":          "fitpolicyrules",
 		"hardwareprofile":        "hardwareprofiles",
 		"ip":                     "ips",
 		"license":                "licenses",
+		"limit":                  "limits",
+		"loadbalancer":           "loadbalancers",
 		"machineloadrule":        "machineloadrules",
 		"machine":                "machines",
 		"networkservicetype":     "networkservicetypes",
@@ -28,6 +33,7 @@ func init() {
 		"rack":                   "racks",
 		"remoteservice":          "remoteservices",
 		"role":                   "roles",
+		"scalinggroup":           "scalinggroups",
 		"scope":                  "scopes",
 		"user":                   "users",
 		"vlan":                   "vlans",
@@ -35,9 +41,12 @@ func init() {
 		"virtualdatacenter":      "virtualdatacenters",
 		"virtualmachine":         "virtualmachines",
 		"virtualmachinetemplate": "virtualmachinetemplates",
+		"volume":                 "volumes",
 	}
 
 	resources := map[string]func() core.Resource{
+		"alarm":                  func() core.Resource { return new(Alarm) },
+		"alert":                  func() core.Resource { return new(Alert) },
 		"backuppolicy":           func() core.Resource { return new(BackupPolicy) },
 		"category":               func() core.Resource { return new(Category) },
 		"datacenter":             func() core.Resource { return new(Datacenter) },
@@ -49,10 +58,13 @@ func init() {
 		"devicetype":             func() core.Resource { return new(DeviceType) },
 		"enterprise":             func() core.Resource { return new(Enterprise) },
 		"enterpriseproperties":   func() core.Resource { return new(EnterpriseProperties) },
+		"firewallpolicy":         func() core.Resource { return new(Firewall) },
 		"fitpolicyrule":          func() core.Resource { return new(FitPolicy) },
 		"hardwareprofile":        func() core.Resource { return new(HardwareProfile) },
 		"ip":                     func() core.Resource { return new(IP) },
 		"license":                func() core.Resource { return new(License) },
+		"limit":                  func() core.Resource { return new(Limit) },
+		"loadbalancer":           func() core.Resource { return new(LoadBalancer) },
 		"machine":                func() core.Resource { return new(Machine) },
 		"machineloadrule":        func() core.Resource { return new(MachineLoadRule) },
 		"networkservicetype":     func() core.Resource { return new(NetworkServiceType) },
@@ -61,6 +73,7 @@ func init() {
 		"rack":                   func() core.Resource { return new(Rack) },
 		"remoteservice":          func() core.Resource { return new(RemoteService) },
 		"role":                   func() core.Resource { return new(Role) },
+		"scalinggroup":           func() core.Resource { return new(ScalingGroup) },
 		"scope":                  func() core.Resource { return new(Scope) },
 		"user":                   func() core.Resource { return new(User) },
 		"vlan":                   func() core.Resource { return new(Network) },
@@ -68,6 +81,7 @@ func init() {
 		"virtualdatacenter":      func() core.Resource { return new(VirtualDatacenter) },
 		"virtualmachine":         func() core.Resource { return new(VirtualMachine) },
 		"virtualmachinetemplate": func() core.Resource { return new(VirtualMachineTemplate) },
+		"volume":                 func() core.Resource { return new(Volume) },
 	}
 
 	for media, factory := range resources {
