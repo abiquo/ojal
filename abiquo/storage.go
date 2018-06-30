5b771cb1ad1d61f9537cd26dfa6a1b3268234fed
antxon <antxon.gonzalez@abiquo.com>
2018-06-30 18:19:50 +0200

    Add storage devices

---
+++ abiquo/storage.go
@@ -0,0 +1,27 @@
+package abiquo
+
+import "github.com/abiquo/ojal/core"
+
+type StorageDevice struct {
+	ID             int64  `json:"id,omitempty"`
+	Name           string `json:"name"`
+	Technology     string `json:"storageTechnology"`
+	ManagementIP   string `json:"managementIp"`
+	ManagementPort int64  `json:"managementPort"`
+	ServiceIP      string `json:"serviceIp"`
+	ServicePort    int64  `json:"servicePort"`
+	core.DTO
+}
+
+type StoragePool struct {
+	IDStorage         string `json:"idStorage,omitempty"`
+	Name              string `json:"name"`
+	Type              string `json:"type"`
+	TotalSizeInMB     int64  `json:"totalSizeInMb"`
+	UsedSizeInMB      int64  `json:"usedSizeInMb"`
+	AvailableSizeInMB int64  `json:"availableSizeInMb"`
+	Enabled           bool   `json:"enabled"`
+	MaxVolumes        int64  `json:"maxVolumes"`
+	UsablePercent     int64  `json:"usablePercent"`
+	core.DTO
+}
