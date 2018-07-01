4cd6c5e3f612de0992a02b9e49ce613b18aa7f75
antxon <antxon.gonzalez@abiquo.com>
2018-07-01 19:51:14 +0200

    Extend limit DTO

--- abiquo/limit.go
+++ abiquo/limit.go
@@ -2,21 +2,22 @@ package abiquo
 
 import "github.com/abiquo/ojal/core"
 
+// Limit represents an abiquo enterprise limit
 type Limit struct {
-	CPUSoft  int `json:"cpuSoft"`
-	HDSoft   int `json:"HDSoft"`
-	IPSoft   int `json:"ipSoft"`
-	RAMSoft  int `json:"ramSoft"`
-	RepoSoft int `json:"repositorySoftInMb"`
-	VolSoft  int `json:"volSoft"`
-	VLANSoft int `json:"vlansSoft"`
-
-	CPUHard  int `json:"cpuHard"`
-	HDHard   int `json:"HDHard"`
-	IPHard   int `json:"ipHard"`
-	RAMHard  int `json:"ramHard"`
-	RepoHard int `json:"repositoryHardInMb"`
-	VolHard  int `json:"volHard"`
-	VLANHard int `json:"vlansHard"`
+	CPUHard   int  `json:"cpuHard"`
+	CPUSoft   int  `json:"cpuSoft"`
+	HDHard    int  `json:"HDHard"`
+	HDSoft    int  `json:"HDSoft"`
+	EnableHPs bool `json:"enabledHardwareProfiles"`
+	IPHard    int  `json:"ipHard"`
+	IPSoft    int  `json:"ipSoft"`
+	RAMHard   int  `json:"ramHard"`
+	RAMSoft   int  `json:"ramSoft"`
+	RepoHard  int  `json:"repositoryHardInMb"`
+	RepoSoft  int  `json:"repositorySoftInMb"`
+	VLANHard  int  `json:"vlansHard"`
+	VLANSoft  int  `json:"vlansSoft"`
+	VolHard   int  `json:"volHard"`
+	VolSoft   int  `json:"volSoft"`
 	core.DTO
 }
